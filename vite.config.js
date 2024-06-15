d8d998acd9667a2b9bbdb953e70b118680540f01
Afiq Hasif <afiqhasifahmadilmi@airasia.com>
2024-06-15 22:52:39 +0800

    chore: project config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  base: "/mdc/",
+  resolve: {
+    alias: [
+      { find: "views", replacement: fileURLToPath(new URL("./src/views", import.meta.url)) },
+      { find: "common", replacement: fileURLToPath(new URL("./src/common", import.meta.url)) },
+      { find: "assets", replacement: fileURLToPath(new URL("./src/assets", import.meta.url)) },
+      { find: "layouts", replacement: fileURLToPath(new URL("./src/layouts", import.meta.url)) },
+      { find: "reducers", replacement: fileURLToPath(new URL("./src/common/redux/reducers", import.meta.url)) },
+    ],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "build/",
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo?.name?.split(".")?.[1] ?? "";
+          if (/png|jpeg|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `static/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "static/js/[name]-[hash].js",
+        entryFileNames: "static/js/[name]-[hash].js",
+      },
+    },
+  },
+});
