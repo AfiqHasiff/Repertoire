e7b0c65aa88027392e8c2dfb46a50ad4ffaf1a14
Afiq Hasif <afiqhasifahmadilmi@airasia.com>
2024-06-16 22:05:24 +0800

    chore: added todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { ThreeDCardComponent, MacbookScrollComponent, LampComponent } from "./layouts";
 import "./App.css";
 
+// TODO: Fonts
+// TODO: Relative pathing config
+
 const App = () => {
   return (
     <div id="appWrapper">
       <LampComponent />
       <MacbookScrollComponent />
+      <ThreeDCardComponent />
       <ThreeDCardComponent />
     </div>
   );
